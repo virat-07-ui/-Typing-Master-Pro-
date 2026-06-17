d0fa311eb75dcf228e49a4aecb87591c6508831a
Kaushal <kaushalchaudhary.10101@gmail.com>
2026-06-17 10:41:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -380,3 +380,14 @@ function startZombieMode(){
     },30);
 
 }
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        document
+        .getElementById("splash")
+        .style.display = "none";
+
+    }, 3000);
+
+});
